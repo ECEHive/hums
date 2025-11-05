b048a23fa71f59280ccac6c2a2a44f0f0f0ca488
Andrew Lemons <github@aglemons.com>
2025-11-05 19:12:15 +0000

    Update client and kiosk Vite configs

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,28 +2,19 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import router from "@tanstack/router-plugin/vite";
-import basicSSL from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), ["VITE_", "PROXY_"]);
+	const env = loadEnv(mode, process.cwd());
 
 	return {
 		publicDir: "public",
+		base: env.VITE_PUBLIC_PATH ?? "/",
 		server: {
 			port: 44831,
 			host: "0.0.0.0",
-			https: {},
-			proxy: {
-				"/api": {
-					target: env.PROXY_PRIVATE_SERVER_URL,
-					changeOrigin: true,
-				},
-				// If more server endpoints are added,
-				// they can be proxied here.
-			},
 		},
 		plugins: [
 			devtools({
@@ -38,7 +29,6 @@ export default defineConfig(({ mode }) => {
 			}),
 			react(),
 			tailwindcss(),
-			basicSSL(),
 		],
 		resolve: {
 			alias: {
