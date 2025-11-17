6f03207f64b1906160a7942905cc95f1ad44d018
Andrew Lemons <github@aglemons.com>
2025-11-17 15:07:38 +0000

    Switch to Day JS for better date and time handling

--- apps/kiosk/src/env.d.ts
+++ apps/kiosk/src/env.d.ts
@@ -1,1 +1,13 @@
 declare const __APP_VERSION__: string;
+
+declare global {
+	interface ImportMetaEnv {
+		readonly TZ: string;
+	}
+
+	interface ImportMeta {
+		readonly env: ImportMetaEnv;
+	}
+}
+
+export {};
